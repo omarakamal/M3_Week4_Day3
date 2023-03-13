75c6e76449cd6a9a98fef37f2d6be826d77893ec
Omar <omaraakamal@gmail.com>
2023-03-13 08:39:42 +0400

    Full Stack

---
+++ project-management-client/src/pages/ProjectList.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+
+function ProjectList() {
+    const [projects,setProjects] = useState([])
+
+    useEffect(()=>{
+        axios.get('http://localhost:5005/api/projects')
+        .then(response=>{
+            console.log(response.data)
+          
+                setProjects(response.data)
+
+        })
+    },[])
+  return (
+    <div>
+       
+       {projects.length===0 && <h2>Loading...</h2>}
+       
+        {projects.length>0 && projects.map(individualProject=>{
+            return(
+                <div key={individualProject._id}>
+                    <h2>{individualProject.title}</h2>
+                    <p>{individualProject.description}</p>
+                    <Link to={`/projects/${individualProject._id}`}>Link to Project</Link>
+                    <br />
+                    <br />
+                    <Link to={`/projects/edit/${individualProject._id}`}>Edit Project</Link>
+                </div>
+            )
+        })}
+    </div>
+  )
+}
+
+export default ProjectList
